93c3489d78a6ad70f70343f552adc0fb7efaf008
Ayush Tiwari <55916664+Ayush144@users.noreply.github.com>
2022-08-17 21:52:01 +0530

    Create Decrypt String from Alphabet to Integer Mapping.java

---
+++ Programming Skill 1/DAY 9/Decrypt String from Alphabet to Integer Mapping.java
@@ -0,0 +1,45 @@
+You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
+
+Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+Return the string formed after mapping.
+
+The test cases are generated so that a unique mapping will always exist.
+
+ 
+
+Example 1:
+
+Input: s = "10#11#12"
+Output: "jkab"
+Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+Example 2:
+
+Input: s = "1326#"
+Output: "acz"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of digits and the '#' letter.
+s will be a valid string such that mapping is always possible.
+=============================================================================================================================================================================
+JAVA SOLUTION
+class Solution {
+    public String freqAlphabets(String s) {
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i =s.length()-1;0<=i;i--){
+            if(s.charAt(i) == '#'){
+                sb.append((char)('a' + (s.charAt(i-1) -'0' ) + 10*(s.charAt(i-2)-'0') -1) );
+                i -= 2;
+            } else {
+                sb.append((char)('a' + (s.charAt(i) - '0') -1));
+            }
+        }
+        sb.reverse();
+        return sb.toString();
+     
+    }
+}
